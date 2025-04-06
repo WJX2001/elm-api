f581405b85ab4edeb9ea4311c98069026f485d5c
WJX2001 <1758807220@qq.com>
2025-04-07 07:31:43 +0800

    feat: 获取商铺列表API

--- routes/v2.js
+++ routes/v2.js
@@ -1,8 +1,9 @@
 import express from 'express'
+import Entry from '../controller/v2/entry'
 import CityHandle from '../controller/v1/cities'
 const router = express.Router()
 
-
+router.get('/index_entry', Entry.getEntry);
 router.get('/pois/:geohash', CityHandle.pois)
 
 
