108105038b748c128d8f21e6e1491361b004b37f
WJX2001 <1758807220@qq.com>
2025-04-09 07:48:56 +0800

    feat: 获取配送方式

--- controller/shopping/category.js
+++ controller/shopping/category.js
@@ -1,24 +1,61 @@
 import BaseComponent from "../../prototype/baseComponent"
 import CategoryModel from '../../models/shopping/category'
-
+import DeliveryModel from '../../models/shopping/delivery'
 
 class Category extends BaseComponent {
-  constructor() {
-    super()
-  }
+	constructor() {
+		super()
+	}
+
+	/**
+	 * 获取所有餐馆分类和数量
+	 * GET /shopping/v2/restaurant/category
+	 */
+	async getCategories (req, res, next) {
+		try {
+			const categories = await CategoryModel.find({}, '-_id')
+			res.send(categories)
+		} catch (err) {
+			console.log('获取categories失败')
+			res.send({
+				status: 0,
+				type: 'ERROR_DATA',
+				message: '获取categories失败'
+			})
+		}
+	}
 
 
-  async findById(id){
-		try{
-			const CateEntity = await CategoryModel.findOne({'sub_categories.id': id});
-			let categoName = CateEntity.name;
+	/**
+	 * 获取配送方式
+	 * GET /shopping/v1/restaurants/delivery_modes
+	 */
+	async getDelivery (req, res, next) {
+		try {
+			const deliveries = await DeliveryModel.find({}, '-_id')
+			res.send(deliveries)
+		} catch (err) {
+			console.log('获取配送方式数据失败')
+			res.send({
+				status: 0,
+				type: 'ERROR_DATA',
+				message: '获取配送方式数据失败'
+			})
+		}
+	}
+
+
+	async findById (id) {
+		try {
+			const CateEntity = await CategoryModel.findOne({ 'sub_categories.id': id })
+			let categoName = CateEntity.name
 			CateEntity.sub_categories.forEach(item => {
 				if (item.id == id) {
-					categoName += '/' + item.name;
+					categoName += '/' + item.name
 				}
 			})
 			return categoName
-		}catch(err){
+		} catch (err) {
 			console.log('通过category id获取数据失败')
 			throw new Error(err)
 		}
--- routes/shopping.js
+++ routes/shopping.js
@@ -1,9 +1,10 @@
-import express from 'express';
+import express from 'express'
 import Shop from '../controller/shopping/shop'
-const router = express.Router();
+import Category from '../controller/shopping/category'
+const router = express.Router()
 
-router.get('/restaurants',Shop.getRestaurants)
+router.get('/restaurants', Shop.getRestaurants)
+router.get('/v2/restaurant/category', Category.getCategories)
+router.get('/v1/restaurants/delivery_modes', Category.getDelivery)
 
-
-
-export default router;
+export default router
