70cf9f8403e9e0ce3e6a59895294103fd43c980d
WJX2001 <1758807220@qq.com>
2025-04-18 08:20:57 +0800

    feat: 获取订单列表

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@
 import v1 from './v1'
 import v2 from './v2'
 import v4 from './v4'
+import bos from './bos'
 import shopping from './shopping'
 export default app => {
   app.use('/v1', v1)
   app.use('/v2', v2)
   app.use('/v4', v4)
   app.use('/shopping', shopping)
+  app.use('/bos', bos)
 }
 
